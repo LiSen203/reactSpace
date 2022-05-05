80d56a84c70a207fc94498706c2fdbd1024cbed6
LiSen203 <1511156517@qq.com>
2022-05-06 00:06:34 +0800

    update

--- reactMtDemo/src/App.js
+++ reactMtDemo/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import Index from './views/Home/index'
 import React,{Component} from "react";
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 // import axios from 'axios';
 export default class App extends Component{
-
-  state={}
-  changeValue =(event,value)=>{
-    this.setState({
-      [value]:event.target.value
-    })
-    console.log(this.state.changetest)
-  }
+// state={}
+  // changeValue =(event,value)=>{
+  //   this.setState({
+  //     [value]:event.target.value
+  //   })
+  //   console.log(this.state.changetest)
+  // }
   render(){
     return (
-      <div className="App">
+      <div className='App'>
         <Index></Index>
       </div>
-    );
+    )
   }
 }
+
--- reactMtDemo/src/views/Home/index.jsx
+++ reactMtDemo/src/views/Home/index.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import './index.css'
 import Header from '../../components/Header'
-export default class idnex extends Component{
-    state ={
+// import Recommends from '../../components/Recommends'
+import {  Carousel  } from 'antd'
+export default class index extends Component {
+    state={
+        // img:`${require('../../static/imgs/logo.png')}`
         CarouselImg:[
             "http://p1.meituan.net/codeman/826a5ed09dab49af658c34624d75491861404.jpg",
             "http://p0.meituan.net/codeman/33ff80dc00f832d697f3e20fc030799560495.jpg",
@@ -111,81 +114,76 @@ export default class idnex extends Component{
             type:'hotal',
             bgc:'linear-gradient(to right, rgb(243, 182, 74) 2%, rgb(242, 197, 69) 97%) rgb(243, 182, 74)'
         }
-        
     }
-    render(){
-        return(
+    render() {
+        return (
             <div id="index">
-            {/* <Header></Header> */}
-            <div className='searchCon'>
-                <div className='logoCon'>
-                    <img className='logo' src="https://s0.meituan.net/bs/fe-web-meituan/10afbf1/img/logo.png" alt="" srcset="" />
-                </div>
-                <div className='search'>
-                    <input className='searchInput' type="text" placeholder='搜索商家或地点'/>
-                    <div className='searchButton'>搜索</div>
-                </div>
-            </div>
-            <div className='clearFix'></div>
-            <div className='main'>
-                <div className='leftNav'>
-                    <div className='title'>全部分类</div>
-                    <div className='serviceList'>
-                        <ul>
-                            <li><i className='iconfontNew hc-icon-foodNew'></i>美食</li>
-                            <li>外卖</li>
-                            <li>酒店</li>
-                            <li>民宿</li>
-                            <li>猫眼电影</li>
-                        </ul>
+                <Header></Header>
+                <div className='searchCon'>
+                    <div className='logoCon'>
+                        <img className='logo' src="https://s0.meituan.net/bs/fe-web-meituan/10afbf1/img/logo.png" alt="" srcset="" />
+                    </div>
+                    <div className='search'>
+                        <input className='searchInput' type="text" placeholder='搜索商家或地点'/>
+                        <div className='searchButton'>搜索</div>
                     </div>
                 </div>
-                <div className='titles'>
-                    <div className='item1'>美团外卖</div>
-                    <div className='item2'>猫眼电影</div>
-                    <div className='item3'>美团酒店</div>
-                    <div className='item4'>民宿/公寓</div>
-                    <div className='item5'>商家入驻</div>
-                    <div className='item6'>美团公益</div>
-                </div>
-                <div className='adImgs'>
-                    {/* <Carousel autoplay>
-                        {
-                            this.state.CarouselImg.map((item,index)=>{
-                                return (
-                                    <div className='item' key={index}>
-                                        <img src={item} alt="" srcset="" />
-                                    </div>
-                                )
-                            })
-                        }
-                    </Carousel> */}
-                    <img className='songImg' src="http://p0.meituan.net/codeman/e473bb428f070321269b23370ff02ba956209.jpg" alt="" />
-                    <div className='loginCon'>
-                        <div className='logoCon'>
-                            <img className='logo' src="https://s0.meituan.net/bs/fe-web-meituan/464c015/img/avatar.jpg" alt="" />
+                <div className='clearFix'></div>
+                <div className='main'>
+                    <div className='leftNav'>
+                        <div className='title'>全部分类</div>
+                        <div className='serviceList'>
+                            <ul>
+                                <li><i className='iconfontNew hc-icon-foodNew'></i>美食</li>
+                                <li>外卖</li>
+                                <li>酒店</li>
+                                <li>民宿</li>
+                                <li>猫眼电影</li>
+                            </ul>
                         </div>
-                        <p className='welcome'>Hi！你好</p>
-                        <div className='button'>注册</div>
-                        <div className='button'>立即登录</div>
                     </div>
-                    <img style={{width:'270px',height:'165px',margin:'10px 10px 0 24px'}} src="http://p1.meituan.net/codeman/8cce56c467a17e04f3094d1e455462a0132772.png" alt=""/>
-                    <img style={{width:'270px',height:'165px',margin:'10px 10px 0 0'}} src="http://p1.meituan.net/codeman/16442c19da1f1c4544f794e29d99c92051716.jpg" alt=""/>
-                    <img style={{width:'150px',height:'165px',margin:'10px 10px 0 0'}} src="http://p1.meituan.net/codeman/5b21cddb4bb1cbc3a9c3bce0f726c75940469.jpg" alt=""/>
-                    <div className='shareCon'>
-                        {/* <QRCode
-                            id="bill_qr_code_url"
-                            value='xxx' //value参数为生成二维码的链接 我这里是由后端返回
-                            size={95} //二维码的宽高尺寸
-                            fgColor="#000000"></QRCode> */}
-                        <p className='AppText'>美团APP手机版</p>
-                        <p className='Ads'><span style={{color:'#EC5300'}}>1元起</span>吃喝玩乐</p>
+                    <div className='titles'>
+                        <div className='item1'>美团外卖</div>
+                        <div className='item2'>猫眼电影</div>
+                        <div className='item3'>美团酒店</div>
+                        <div className='item4'>民宿/公寓</div>
+                        <div className='item5'>商家入驻</div>
+                        <div className='item6'>美团公益</div>
+                    </div>
+                    <div className='adImgs'>
+                        <Carousel autoplay>
+                            {
+                                this.state.CarouselImg.map((item,index)=>{
+                                    return (
+                                        <div className='item' key={index}>
+                                            <img src={item} alt="" srcset="" />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Carousel>
+                        <img className='songImg' src="http://p0.meituan.net/codeman/e473bb428f070321269b23370ff02ba956209.jpg" alt="" />
+                        <div className='loginCon'>
+                            <div className='logoCon'>
+                                <img className='logo' src="https://s0.meituan.net/bs/fe-web-meituan/464c015/img/avatar.jpg" alt="" />
+                            </div>
+                            <p className='welcome'>Hi！你好</p>
+                            <div className='button'>注册</div>
+                            <div className='button'>立即登录</div>
+                        </div>
+                        <img style={{width:'270px',height:'165px',margin:'10px 10px 0 24px'}} src="http://p1.meituan.net/codeman/8cce56c467a17e04f3094d1e455462a0132772.png" alt=""/>
+                        <img style={{width:'270px',height:'165px',margin:'10px 10px 0 0'}} src="http://p1.meituan.net/codeman/16442c19da1f1c4544f794e29d99c92051716.jpg" alt=""/>
+                        <img style={{width:'150px',height:'165px',margin:'10px 10px 0 0'}} src="http://p1.meituan.net/codeman/5b21cddb4bb1cbc3a9c3bce0f726c75940469.jpg" alt=""/>
+                        <div className='shareCon'>
+                           
+                            <p className='AppText'>美团APP手机版</p>
+                            <p className='Ads'><span style={{color:'#EC5300'}}>1元起</span>吃喝玩乐</p>
+                        </div>
                     </div>
                 </div>
+                {/* <Recommends filmObject={this.state.filmObject}></Recommends>
+                <Recommends filmObject={this.state.hotalObject}></Recommends> */}
             </div>
-            {/* <Recommends filmObject={this.state.filmObject}></Recommends>
-            <Recommends filmObject={this.state.hotalObject}></Recommends> */}
-        </div>
         )
     }
-}   
+}
